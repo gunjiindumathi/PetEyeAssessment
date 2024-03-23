f7c0ef74807c3ce64acb5504d0581f5bcdd4ab86
gunjiindumathi <111984708+gunjiindumathi@users.noreply.github.com>
2024-03-23 15:43:55 +0530

    Add files via upload

--- ListOfItems.js
+++ ListOfItems.js
@@ -5,10 +5,10 @@ import { View, Text, StyleSheet, FlatList, Image, ImageComponent, } from 'react-
 const ListOfItems = () => {
 
     const data = [
-        { id: 1, Name: Lakshmi, Qualification: BigInt.tech, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')}},
-        { id: 2, Name: Durga, Qualification: B.Tech, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')}},
-        { id: 3, Name: Thulasi, Qualification: M.Farm, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')} },
-        { id: 4, Name: Bhargavi, Qualification: B.Farm, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')}}
+        { id: 1, Name: Lakshmi, Qualification: BigInt.tech, image: { URL: ('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1') } },
+        { id: 2, Name: Durga, Qualification: B.Tech, image: { URL: ('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1') } },
+        { id: 3, Name: Thulasi, Qualification: M.Farm, image: { URL: ('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1') } },
+        { id: 4, Name: Bhargavi, Qualification: B.Farm, image: { URL: ('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1') } }
     ];
 
     return (
@@ -21,9 +21,7 @@ const ListOfItems = () => {
                         <Text style={styles.id}>item.id</Text>
                         <Text style={styles.Name}>item.name</Text>
                         <Text style={styles.qualification}>item.Qualification</Text>
-                        <ImageComponent style={styles.image}
-                       
-                        > source={item.image}</ImageComponent>
+                        <ImageComponent style={styles.image}> source={item.image}</ImageComponent>
                     </View>
                 }
             </FlatList>
