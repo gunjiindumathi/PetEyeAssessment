b59f4cb521e385a5869cc726132dc4a33421b536
gunjiindumathi <111984708+gunjiindumathi@users.noreply.github.com>
2024-03-23 15:30:20 +0530

    Add files via upload

--- ListOfItems.js
+++ ListOfItems.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList, } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image, ImageComponent, } from 'react-native';
 
 
 const ListOfItems = () => {
 
     const data = [
-        { id: 1, name: Lakshmi, Qualification: BigInt.tech },
-        { id: 2, name: Durga, Qualification: B.Tech },
-        { id: 3, name: Thulasi, Qualification: M.Farm },
-        { id: 4, name: Bhargavi, Qualification: B.Farm }
+        { id: 1, Name: Lakshmi, Qualification: BigInt.tech, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')}},
+        { id: 2, Name: Durga, Qualification: B.Tech, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')}},
+        { id: 3, Name: Thulasi, Qualification: M.Farm, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')} },
+        { id: 4, Name: Bhargavi, Qualification: B.Farm, image: {URL:('https://www.bing.com/ck/a?!&&p=6c0fb03eca90c10eJmltdHM9MTcxMTE1MjAwMCZpZ3VpZD0wOWM3MDZkZC0zZTI2LTZiMTAtMjZiMi0xNTBlM2Y2ZjZhNzcmaW5zaWQ9NTYwOA&ptn=3&ver=2&hsh=3&fclid=09c706dd-3e26-6b10-26b2-150e3f6f6a77&u=a1L2ltYWdlcy9zZWFyY2g_cT1zdHVkZW50JTIwaWNvbiUyMGltYWdlcyUyMGluJTIwcG5nJTIwJkZPUk09SVFGUkJBJmlkPUMzNjI0QzUzNUVENjUzNTlDQkRCNTIxRDkyQTMzMDEzODFGOTg0NDU&ntb=1')}}
     ];
 
     return (
@@ -18,9 +18,10 @@ const ListOfItems = () => {
                 KeyExtractor= {(item) => (item.id.data)}
                 renderItem= {({ item }) =>
                     <View style={styles.containerView}>
-                        <Text>item.id</Text>
-                        <Text>item.name</Text>
-                        <Text>item.Qualification</Text>
+                        <Text style={styles.id}>item.id</Text>
+                        <Text style={styles.Name}>item.name</Text>
+                        <Text style={styles.qualification}>item.Qualification</Text>
+                        <ImageComponent style={styles.image}>item.image</ImageComponent>
                     </View>
                 }
             </FlatList>
@@ -36,5 +37,25 @@ const styles = StyleSheet.create({
     },
     flatlistItems: {
         alignItems: 'center',
+    },
+    id: {
+        color: 'black',
+        fontStyle: 'normal',
+        fontSize: 12
+    },
+    Name: {
+        color: 'black',
+        fontStyle: 'normal',
+        fontSize: 14
+    },
+    qualificationualification: {
+        color: 'black',
+        fontStyle: 'normal',
+        fontSize: 14
+    },
+    image: {
+        height: 60,
+        width: 60,
+        marginHorizontal: 5
     }
 });
